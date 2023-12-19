9530ca2be252d3c8d26f83dd8816992c495a2d8c
Narendra725 <132801604+Narendra725@users.noreply.github.com>
2023-12-19 23:12:01 +0530

    Update incremental_data_load.py
    updated .json extension for credits file

--- incremental_data_load.py
+++ incremental_data_load.py
@@ -4,7 +4,7 @@ from pyspark.sql.types import *
 from google.cloud import storage
 from datetime import datetime
 
-def load_csv_for_table_from_bucket(bucket_name,table_name):
+def load_df_for_table_from_bucket(bucket_name,table_name,ext):
         def fetch_last_run_and_update_last_run(bucket_name,table_name):
             last_run=spark.read.format("csv").option("recursiveFileLookup", "true") \
                                              .option("header", "true") \
@@ -15,21 +15,21 @@ def load_csv_for_table_from_bucket(bucket_name,table_name):
 
             return last_run.collect()[0][0]
             
-        def list_csv_blobs(bucket_name,last_run_time,table_name,csv_files_list=None):
-                if csv_files_list is None:
-                    csv_files_list=[]
+        def list_blobs(bucket_name,last_run_time,table_name,files_list=None):
+                if files_list is None:
+                    files_list=[]
                 client = storage.Client()
                 bucket = client.get_bucket(bucket_name)
                 blobs = bucket.list_blobs()
                 for blob in blobs:
-                    if blob.updated.replace(tzinfo=None) > last_run and blob.name.startswith(f"{table_name}/") and blob.name.endswith(".csv"):
+                    if blob.updated.replace(tzinfo=None) > last_run and blob.name.startswith(f"{table_name}/") and blob.name.endswith(f".{ext}"):
                         if blob.name.startswith(f"{table_name}/last_run/"):
-                            continue  # Skip csv_blobs within "last_run" folder
+                            continue  # Skip blobs within "last_run" folder
                         else:
-                            csv_files_list.append(f"gs://{bucket_name}/{blob.name}")
-                return csv_blobs_list
-        def read_csv_to_dataframe(filepath):
-            df = spark.read.format("csv") \
+                            files_list.append(f"gs://{bucket_name}/{blob.name}")
+                return files_list
+        def read_to_dataframe(filepath,ext):
+            df = spark.read.format(ext) \
             .option("header", "true") \
             .option("inferSchema", "true") \
             .load(file_path)
@@ -43,10 +43,14 @@ def load_csv_for_table_from_bucket(bucket_name,table_name):
                     combined_df = combined_df.union(df)
             return combined_df
         dataframes=[]
-        for file in list_csv_blobs(bucket_name,fetch_last_run_and_update_last_run(bucket_name,table_name),table_name):
-            dataframes.append(read_csv_to_dataframe(file))
+        for file in list_blobs(bucket_name,fetch_last_run_and_update_last_run(bucket_name,table_name),table_name):
+            dataframes.append(read_to_dataframe(file))
         return combine_dataframes_to_single_df(dataframes)
 dfs={}
 for table in ["customers","transactions","accounts","branches","loans","credits"]:
-  dfs[table]=load_csv_for_table_from_bucket('bronze-layer-capstone",table)
+        if table=="credits":
+                ext="json"
+        else:
+                ext="csv"
+        dfs[table]=load_df_for_table_from_bucket('bronze-layer-capstone",table,ext)
   
