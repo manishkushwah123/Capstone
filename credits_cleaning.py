6552ac35a05bc6db052331952104b74d8b946ff5
Narendra725 <132801604+Narendra725@users.noreply.github.com>
2023-12-20 22:06:34 +0530

    Update credits_cleaning.py

--- credits_cleaning.py
+++ credits_cleaning.py
@@ -1,18 +1,7 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.0
-#   kernelspec:
-#     display_name: PySpark (Local)
-#     language: python
-#     name: pyspark
-# ---
-
-# +
+from pyspark.sql import SparkSession
+import time
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 import pyspark
 from pyspark.sql.window import *
 from pyspark.sql.functions import *
@@ -20,31 +9,33 @@ from pyspark.sql.types import *
 from google.cloud import storage
 from datetime import datetime
 
-schema=StructType([
-    StructField("Customer_id", StringType(), True),
-    StructField("Credit Score", DoubleType(), True),
-    StructField("Annual Income", DoubleType(), True),
-    StructField("Years of Credit History", DoubleType(), True),
-    StructField("Number of Open Accounts", DoubleType(), True),
-    StructField("Number of Credit Problems", DoubleType(), True),
-    StructField("Current Credit Balance", DoubleType(), True),
-    StructField("Maximum Open Credit", DoubleType(), True),
-    StructField("Bankruptcies", DoubleType(), True),
-    StructField("Tax Liens", DoubleType(), True),
-    StructField("Months since last delinquent", DoubleType(), True)  # Add "Months since last delinquent" field
-])
-
 def load_df_for_table_from_bucket(bucket_name,table_name,ext,schema):
-        def fetch_last_run_and_update_last_run(bucket_name,table_name):
+        timestamp=datetime.utcnow()
+        
+        def delete_files_from_folder(bucket_name,folder_name):
+            from google.cloud import storage
+            client = storage.Client()
+            bucket = client.get_bucket(bucket_name)
+            blobs = bucket.list_blobs(prefix=folder_name)
+            for blob in blobs:
+                if blob.name!= folder_name:
+                        blob.delete()
+                        print(f"Deleted blob: {blob.name}")
+        def update_last_run(timestamp):
+            delete_files_from_folder(bucket_name,f"{table_name}/last_run/")
+            data = [(timestamp,)]
+            columns = ["last_run"]
+            df = spark.createDataFrame(data, columns)
+            df.write.csv(f"gs://{bucket_name}/{table_name}/last_run/", header=True, mode="append")
+        def read_last_run():
             last_run=spark.read.format("csv").option("recursiveFileLookup", "true") \
                                              .option("header", "true") \
                                              .option("inferSchema", "true") \
                                              .load(f"gs://{bucket_name}/{table_name}/last_run/*.csv")
-            last_run = last_run.withColumn("Last_run_timestamp", current_timestamp())
-            last_run.write.format("csv").mode("overwrite").option("header","true").save(f"gs://{bucket_name}/{table_name}/last_run/")
 
             return last_run.collect()[0][0]
             
+        
         def list_blobs(bucket_name,last_run_time,table_name,files_list=None):
                 if files_list is None:
                     files_list=[]
@@ -52,13 +43,14 @@ def load_df_for_table_from_bucket(bucket_name,table_name,ext,schema):
                 bucket = client.get_bucket(bucket_name)
                 blobs = bucket.list_blobs()
                 for blob in blobs:
-                    if blob.updated.replace(tzinfo=None) > last_run and blob.name.startswith(f"{table_name}/") and blob.name.endswith(f".{ext}"):
+                    if blob.updated.replace(tzinfo=None) > last_run_time and blob.name.startswith(f"{table_name}/") and blob.name.endswith(f".{ext}"):
                         if blob.name.startswith(f"{table_name}/last_run/"):
                             continue  # Skip blobs within "last_run" folder
                         else:
                             files_list.append(f"gs://{bucket_name}/{blob.name}")
+                print(files_list)
                 return files_list
-        def read_to_dataframe(filepath,ext):
+        def read_to_dataframe(file_path):
             df = spark.read.format(ext) \
             .option("header", "true") \
             .schema(schema)\
@@ -73,10 +65,24 @@ def load_df_for_table_from_bucket(bucket_name,table_name,ext,schema):
                     combined_df = combined_df.union(df)
             return combined_df
         dataframes=[]
-        for file in list_blobs(bucket_name,fetch_last_run_and_update_last_run(bucket_name,table_name),table_name):
+        for file in list_blobs(bucket_name,read_last_run(),table_name):
             dataframes.append(read_to_dataframe(file))
+        update_last_run(timestamp)
         return combine_dataframes_to_single_df(dataframes)
 
+schema=StructType([
+    StructField("Customer_id", StringType(), True),
+    StructField("Credit Score", DoubleType(), True),
+    StructField("Annual Income", DoubleType(), True),
+    StructField("Years of Credit History", DoubleType(), True),
+    StructField("Number of Open Accounts", DoubleType(), True),
+    StructField("Number of Credit Problems", DoubleType(), True),
+    StructField("Current Credit Balance", DoubleType(), True),
+    StructField("Maximum Open Credit", DoubleType(), True),
+    StructField("Bankruptcies", DoubleType(), True),
+    StructField("Tax Liens", DoubleType(), True),
+    StructField("Months since last delinquent", DoubleType(), True)  # Add "Months since last delinquent" field
+])
 
 
 
@@ -97,9 +103,6 @@ credits_csv=credits_csv.select(
     "Tax Liens"
 )
 
-## to check for the presence of missing/null valuesin the customer_id column
-missing_ids = credits_csv.select("Customer_id").subtract(customers_csv.select("Customer_id"))
-print(f"The total customer_ids that are not present in customers_csv is : {missing_ids.count()}")
 
 
 ## droping the duplicated rows
@@ -126,6 +129,6 @@ credits_csv=credits_csv.na.fill(0,subset=["Years of Credit History",
                                           "Tax Liens"])
 
 
-
+credits_csv.show()
 desired_file_path="gs://silver-layer-capstone/credits/"
 credits_csv.write.csv(desired_file_path,header=True,mode="append")
