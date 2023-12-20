6ca70b21ef2e43261924838f58e59b5875dc978d
Narendra725 <132801604+Narendra725@users.noreply.github.com>
2023-12-20 15:52:29 +0530

    Update Customers_cleaning.py

--- Customers_cleaning.py
+++ Customers_cleaning.py
@@ -16,8 +16,68 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-# Read data from a dfs dictionary
-customers_df=dfs["customers"]
+import pyspark
+from pyspark.sql.window import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from google.cloud import storage
+from datetime import datetime
+
+schema = StructType([
+    StructField("Customer_id", StringType(), True),
+    StructField("Full_Name", StringType(), True),
+    StructField("Customer_Email", StringType(), True),
+    StructField("dob", DateType(), True),  # Convert to DateType
+    StructField("Customer_Phone", IntegerType(), True)  # Convert to IntegerType
+])
+
+def load_df_for_table_from_bucket(bucket_name,table_name,ext,schema):
+        def fetch_last_run_and_update_last_run(bucket_name,table_name):
+            last_run=spark.read.format("csv").option("recursiveFileLookup", "true") \
+                                             .option("header", "true") \
+                                             .option("inferSchema", "true") \
+                                             .load(f"gs://{bucket_name}/{table_name}/last_run/*.csv")
+            last_run = last_run.withColumn("Last_run_timestamp", current_timestamp())
+            last_run.write.format("csv").mode("overwrite").option("header","true").save(f"gs://{bucket_name}/{table_name}/last_run/")
+
+            return last_run.collect()[0][0]
+            
+        def list_blobs(bucket_name,last_run_time,table_name,files_list=None):
+                if files_list is None:
+                    files_list=[]
+                client = storage.Client()
+                bucket = client.get_bucket(bucket_name)
+                blobs = bucket.list_blobs()
+                for blob in blobs:
+                    if blob.updated.replace(tzinfo=None) > last_run and blob.name.startswith(f"{table_name}/") and blob.name.endswith(f".{ext}"):
+                        if blob.name.startswith(f"{table_name}/last_run/"):
+                            continue  # Skip blobs within "last_run" folder
+                        else:
+                            files_list.append(f"gs://{bucket_name}/{blob.name}")
+                return files_list
+        def read_to_dataframe(filepath,ext):
+            df = spark.read.format(ext) \
+            .option("header", "true") \
+            .schema(schema)\
+            .load(file_path)
+            return df
+        def combine_dataframes_to_single_df(dataframes):
+            combined_df = None
+            for df in dataframes:
+                if combined_df is None:
+                    combined_df = df
+                else:
+                    combined_df = combined_df.union(df)
+            return combined_df
+        dataframes=[]
+        for file in list_blobs(bucket_name,fetch_last_run_and_update_last_run(bucket_name,table_name),table_name):
+            dataframes.append(read_to_dataframe(file))
+        return combine_dataframes_to_single_df(dataframes)
+
+
+
+
+customers_df = load_df_for_table_from_bucket("bronze-layer-capstone","customers","csv",schema)
 
 cleaned_data = customers_df.withColumn('dob', to_date('dob', 'M/d/yyyy'))
 
