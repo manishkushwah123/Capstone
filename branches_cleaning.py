71cbbf380349763a88711cba0b1e53640fac5528
Narendra725 <132801604+Narendra725@users.noreply.github.com>
2023-12-20 21:58:09 +0530

    Update branches_cleaning.py
    can be pushed to GCP

--- branches_cleaning.py
+++ branches_cleaning.py
@@ -1,16 +1,7 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.0
-#   kernelspec:
-#     display_name: PySpark (Local)
-#     language: python
-#     name: pyspark
-# ---
+from pyspark.sql import SparkSession
+import time
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 import pyspark
 from pyspark.sql.window import *
 from pyspark.sql.functions import *
@@ -18,26 +9,33 @@ from pyspark.sql.types import *
 from google.cloud import storage
 from datetime import datetime
 
-schema = StructType([
-    StructField("BranchId", StringType(), True),
-    StructField("Bank_Name", StringType(), True),
-    StructField("Branch_Registration", StringType(), True),
-    StructField("Bank_city", StringType(), True)
-   
-])
-
-
 def load_df_for_table_from_bucket(bucket_name,table_name,ext,schema):
-        def fetch_last_run_and_update_last_run(bucket_name,table_name):
+        timestamp=datetime.utcnow()
+        
+        def delete_files_from_folder(bucket_name,folder_name):
+            from google.cloud import storage
+            client = storage.Client()
+            bucket = client.get_bucket(bucket_name)
+            blobs = bucket.list_blobs(prefix=folder_name)
+            for blob in blobs:
+                if blob.name!= folder_name:
+                        blob.delete()
+                        print(f"Deleted blob: {blob.name}")
+        def update_last_run(timestamp):
+            delete_files_from_folder(bucket_name,f"{table_name}/last_run/")
+            data = [(timestamp,)]
+            columns = ["last_run"]
+            df = spark.createDataFrame(data, columns)
+            df.write.csv(f"gs://{bucket_name}/{table_name}/last_run/", header=True, mode="append")
+        def read_last_run():
             last_run=spark.read.format("csv").option("recursiveFileLookup", "true") \
                                              .option("header", "true") \
                                              .option("inferSchema", "true") \
                                              .load(f"gs://{bucket_name}/{table_name}/last_run/*.csv")
-            last_run = last_run.withColumn("Last_run_timestamp", current_timestamp())
-            last_run.write.format("csv").mode("overwrite").option("header","true").save(f"gs://{bucket_name}/{table_name}/last_run/")
 
             return last_run.collect()[0][0]
             
+        
         def list_blobs(bucket_name,last_run_time,table_name,files_list=None):
                 if files_list is None:
                     files_list=[]
@@ -45,13 +43,14 @@ def load_df_for_table_from_bucket(bucket_name,table_name,ext,schema):
                 bucket = client.get_bucket(bucket_name)
                 blobs = bucket.list_blobs()
                 for blob in blobs:
-                    if blob.updated.replace(tzinfo=None) > last_run and blob.name.startswith(f"{table_name}/") and blob.name.endswith(f".{ext}"):
+                    if blob.updated.replace(tzinfo=None) > last_run_time and blob.name.startswith(f"{table_name}/") and blob.name.endswith(f".{ext}"):
                         if blob.name.startswith(f"{table_name}/last_run/"):
                             continue  # Skip blobs within "last_run" folder
                         else:
                             files_list.append(f"gs://{bucket_name}/{blob.name}")
+                print(files_list)
                 return files_list
-        def read_to_dataframe(filepath,ext):
+        def read_to_dataframe(file_path):
             df = spark.read.format(ext) \
             .option("header", "true") \
             .schema(schema)\
@@ -66,17 +65,20 @@ def load_df_for_table_from_bucket(bucket_name,table_name,ext,schema):
                     combined_df = combined_df.union(df)
             return combined_df
         dataframes=[]
-        for file in list_blobs(bucket_name,fetch_last_run_and_update_last_run(bucket_name,table_name),table_name):
+        for file in list_blobs(bucket_name,read_last_run(),table_name):
             dataframes.append(read_to_dataframe(file))
+        update_last_run(timestamp)
         return combine_dataframes_to_single_df(dataframes)
-
-
+schema = StructType([
+    StructField("BranchId", StringType(), True),
+    StructField("Bank_Name", StringType(), True),
+    StructField("Branch_Registration", StringType(), True),
+    StructField("Bank_city", StringType(), True)
+   
+])
 
 
 df= load_df_for_table_from_bucket("bronze-layer-capstone","branches","csv",schema)
-
-# -
-
 df_filtered = df.na.drop(subset=["BranchId","Branch_Registration","Bank_Name","Bank_city"])
 
 from pyspark.sql.functions import *
@@ -90,6 +92,4 @@ desired_file_name = "gs://silver-layer-capstone/branches/"
  
 # Write the cleaned DataFrame to a CSV file with the desired file name
 df_transformed.write.csv(desired_file_name, header=True, mode="append")
-# -
-
 
